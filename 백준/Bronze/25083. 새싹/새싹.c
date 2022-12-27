300018daa9ba2b718bfafe9cd5517c25eaaffa7c
yoots50 <102408620+yoots50@users.noreply.github.com>
2022-12-27 21:32:10 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+}
