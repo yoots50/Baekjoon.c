ac00a3bdaa886b257e3455ff7aaba59c0d6f6efe
yoots50 <102408620+yoots50@users.noreply.github.com>
2022-12-28 22:23:54 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+	int L;
+	scanf("%d", &L);
+	char* str = (char*)malloc(sizeof(char) * L);
+	long long int* arr = (long long int*)malloc(sizeof(long long int) * L);
+	scanf("%s", str);
+	long long int sum = 0;
+	for (int i = 0; i < L; i++) {
+		str[i] -= 96;
+		arr[i] = str[i];
+		for (int j = 0; j < i; j++) {
+			arr[i] *= 31;
+			arr[i] -= (long long int)(arr[i] / 1234567891) * 1234567891;
+		}
+		sum += arr[i];
+	}
+	printf("%d", sum % 1234567891);
+}
