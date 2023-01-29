e889bf6e061b386e906b636610c087b3b7152235
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-01-30 00:29:16 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int memo[1001] = { 0, };
+
+int fibonacci(int N) {
+	if (N == 1 || N == 2) {
+		return 1;
+	}
+	if (memo[N] != 0) {
+		return memo[N];
+	}
+	memo[N] = (fibonacci(N - 1) + 2 * fibonacci(N - 2)) % 10007;
+	return memo[N];
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	printf("%d\n", fibonacci(N + 1));
+
+}
