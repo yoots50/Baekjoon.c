e60540f54c5030ce455181776b8807a524a20480
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-02-02 15:57:06 +0900

    [Bronze V] Title: Serca, Time: 136 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,15 @@
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		printf(" @@@   @@@ \n");
+		printf("@   @ @   @\n");
+		printf("@    @    @\n");
+		printf("@         @\n");
+		printf(" @       @ \n");
+		printf("  @     @  \n");
+		printf("   @   @   \n");
+		printf("    @ @    \n");
+		printf("     @     \n");
+	}
+}
