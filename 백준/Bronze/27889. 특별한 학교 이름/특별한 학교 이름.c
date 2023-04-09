fa46b6b8559727cf9b2d28a8ada85b4032495676
yoots50 <102408620+yoots50@users.noreply.github.com>
2023-04-09 21:15:23 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.c
@@ -0,0 +1,17 @@
+int main() {
+	switch (getchar())
+	{
+		case 'N':
+			printf("North London Collegiate School");
+			break;
+		case 'B':
+			printf("Branksome Hall Asia");
+			break;
+		case 'K':
+			printf("Korea International School");
+			break;
+		case 'S':
+			printf("St. Johnsbury Academy");
+			break;
+	}
+}
